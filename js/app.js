9c615f791e17a0d7e81e54941d8b61e7ffcbb1f0
TuanAnh <pes0kloveu123@gmail.com>
2022-07-23 14:43:29 +0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,83 @@
+
+
+$(document).ready(function () {
+    //Owl
+    $('.hero-slider').owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 1,
+        dots: false,
+        navText: ['PREV', 'NEXT'],
+        smartSpeed: 1000,
+        autoplay: true,
+        autoplayTimeout: 7000,
+        responsive: {
+            0: {
+                nav: false,
+            },
+            768: {
+                nav: true,
+            }
+        }
+    })
+
+    $('#projects-slider').owlCarousel({
+        loop: true,
+        nav: false,
+        items: 2,
+        dots: true,
+        smartSpeed: 600,
+        center: true,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2,
+                margin: 8,
+            }
+        }
+    })
+
+    $('.reviews-slider').owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        smartSpeed: 900,
+        items: 1,
+        margin: 24,
+        autoplay: true,
+        autoplayTimeout: 4000,
+    })
+    
+});
+
+const counters = document.querySelectorAll('.counter');
+const speed = 200; // The lower the slower
+
+counters.forEach(counter => {
+    const updateCount = () => {
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+
+        // Lower inc to slow and higher to slow
+        const inc = target / speed;
+
+        // console.log(inc);
+        // console.log(count);
+
+        // Check if target is reached
+        if (count < target) {
+            // Add inc to count and output in counter
+            counter.innerText = count + inc;
+            // Call function every ms
+            setTimeout(updateCount, 1);
+        } else {
+            counter.innerText = target;
+        }
+    };
+
+    updateCount();
+});
